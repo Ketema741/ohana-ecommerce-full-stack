bc540c61e170638a127bdaf08dec2adb1ff73085
KetemagitLab <kgirma363@gmail.com>
2022-04-27 21:58:54 +0300

    adding redux state manager

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <header className="row">
           <div>
             <a className="brand" href="/">
-              bone
+              ohana
             </a>
           </div>
           <div>

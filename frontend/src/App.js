6ac631eb94d78dca64fa5ec3d8fb691aa42e57bd
KetemagitLab <kgirma363@gmail.com>
2022-04-28 23:52:13 +0300

    adding-cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import { Link } from 'react-router-dom';
+
 
 function App() {
+  const cart= useSelector((state)=>state.cart);
+  const {cartItems} = cart;
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               ohana
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">Cart
+            {cartItems.length >0 && (
+              <span className="badge">{cartItems.length}</span>
+            )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>

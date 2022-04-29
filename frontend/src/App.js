eeb370d8f133a9f07eb36982382e8ed0c7dab5f0
KetemagitLab <kgirma363@gmail.com>
2022-04-29 15:08:51 +0300

    adding CartScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-import { Link } from 'react-router-dom';
-
 
 function App() {
-  const cart= useSelector((state)=>state.cart);
-  const {cartItems} = cart;
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -19,15 +19,17 @@ function App() {
             </Link>
           </div>
           <div>
-            <Link to="/cart">Cart
-            {cartItems.length >0 && (
-              <span className="badge">{cartItems.length}</span>
-            )}
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
             </Link>
             <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
